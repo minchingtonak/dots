046c4bd61139c682f786bfb610813a6d2549cb36
Alec <alec.minch@gmail.com>
2019-11-29 19:18:09 -0500

    Reduce code duplication, clone spaceship zsh prompt

--- .yadm/scripts/zsh-plugins.sh
+++ .yadm/scripts/zsh-plugins.sh
@@ -1,14 +1,18 @@
+DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
 # zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $DIR/plugins/zsh-syntax-highlighting
 
 # zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $DIR/plugins/zsh-autosuggestions
 
 # fzf-zsh
-git clone https://github.com/junegunn/fzf.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf
-${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf/install --bin
+git clone https://github.com/junegunn/fzf.git $DIR/plugins/fzf
+$DIR/plugins/fzf/install --bin
 
-git clone https://github.com/Treri/fzf-zsh.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh
+git clone https://github.com/Treri/fzf-zsh.git $DIR/plugins/fzf-zsh
 
 # spaceship custom zsh prompt
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" && ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/denysdovhan/spaceship-prompt.git $DIR/plugins/spaceship-prompt
+ln -s $DIR/plugins/spaceship-prompt/spaceship.zsh-theme $DIR/themes/spaceship.zsh-theme
+
