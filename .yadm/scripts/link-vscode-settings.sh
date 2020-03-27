36c1f1ac07d73f506e8fec405431ab3fe6bdc1e3
Alec Minchington <alec.minch@gmail.com>
2020-03-26 21:56:52 -0400

    vscode config in sensible location

--- .yadm/scripts/link-vscode-settings.sh
+++ .yadm/scripts/link-vscode-settings.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -Eeuxo pipefail
 
 if grep -iq microsoft /proc/version; then
-  cp $HOME/.vscode-settings.json /mnt/c/Users/akmin/AppData/Roaming/Code/User/settings.json
-  cp $HOME/.vscode-keybindings.json /mnt/c/Users/akmin/AppData/Roaming/Code/User/keybindings.json
-else
-  ln -s $HOME/.vscode-settings.json $HOME/.config/Code/User/settings.json
-  ln -s $HOME/.vscode-keybindings.json $HOME/.config/Code/User/keybindings.json
+  cp $HOME/.config/Code/User/settings.json /mnt/c/Users/akmin/AppData/Roaming/Code/User/settings.json
+  cp $HOME/.config/Code/User/keybindings.json /mnt/c/Users/akmin/AppData/Roaming/Code/User/keybindings.json
 fi
 echo "Settings files linked"
