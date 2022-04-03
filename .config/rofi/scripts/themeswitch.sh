d8c67c1a79f1267e316e3d679395bd5ad063faa5
Alec <alec.minch@gmail.com>
2022-04-02 22:09:37 -0400

    add theme switcher rofi script

---
+++ .config/rofi/scripts/themeswitch.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+### Created by ilsenatorov https://github.com/Mocktezuma/dotfiles/blob/master/.config/rofi/scripts/themeswitch.sh
+
+WALLPAPERDIR=~/papes
+
+if [ -z $@ ]
+then
+function get_themes()
+{
+    ls $WALLPAPERDIR
+}
+echo current; get_themes
+else
+    THEMES=$@
+    if [ x"current" = x"${THEMES}" ]
+    then
+        exit 0
+    elif [ -n "${THEMES}" ]
+    then
+        wal --saturate 1 -i "$WALLPAPERDIR/${THEMES}" > /dev/null
+    fi
+fi
