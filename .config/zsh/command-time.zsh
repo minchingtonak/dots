9331fafcb3bbbb509024f454dbd7ce821610854a
Alec <alec.minch@gmail.com>
2022-12-27 11:45:23 -0500

    command time script now prints tenths of seconds

--- .config/zsh/command-time.zsh
+++ .config/zsh/command-time.zsh
@@ -3,14 +3,17 @@
 # Modified by Alec Minchington (github:minchingtonak)
 
 _command_time_preexec() {
-  timer=${timer:-$SECONDS}
+  millis=$(date +%s%3N)
+  timer=${timer:-$millis}
   export ZSH_COMMAND_TIME=""
 }
 
 _command_time_precmd() {
   if [ $timer ]; then
-    timer_show=$(($SECONDS - $timer))
-    if [ -n "$TTY" ] && [ $timer_show -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
+    millis=$(date +%s%3N)
+    timer_show=$(( ($millis - $timer) / 1000.0 ))
+    zmodload zsh/mathfunc
+    if [ -n "$TTY" ] && [ $(( int($timer_show) )) -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
       export ZSH_COMMAND_TIME="$timer_show"
     fi
     unset timer
@@ -19,7 +22,7 @@ _command_time_precmd() {
 
 zsh-command-time() {
     if [ ! -z "$ZSH_COMMAND_TIME" ]; then
-        printf "$1" "$ZSH_COMMAND_TIME"
+      printf "$1" "$ZSH_COMMAND_TIME"
     fi
 }
 
