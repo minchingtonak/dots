095331d5461aadcaba2ee538248a5f2fb1bf868b
Alec Minchington <alec.minch@gmail.com>
2024-10-02 20:44:09 -0700

    add simple bluetooth rofi menu

---
+++ dot_config/rofi/scripts/executable_bluetooth-menu.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+get_options() {
+  bluetoothctl devices Paired | cut -d ' ' -f 2-
+}
+
+connect_or_disconnect() {
+  device=$(echo "$1" | cut -d ' ' -f 1)
+
+  if bluetoothctl info "$device" | grep -iq 'Connected: yes'; then
+    bluetoothctl disconnect "$device"
+  else
+    bluetoothctl connect "$device"
+  fi
+}
+
+main() {
+  # get choice from rofi
+  choice=$( (get_options) | rofi -dmenu -i -fuzzy -p "device")
+
+  # if choice is not empty (will be empty if user presses esc)
+  if [ -n "$choice" ]; then
+    connect_or_disconnect "$choice"
+  fi
+
+  # done
+  set -e
+}
+
+main &
+
+exit 0
